0f343426f9552b3c7d346e0261ba6ec386808013
andrewbloese-00 <andrewbloese@gmail.com>
2024-03-28 15:22:19 -0700

    new map test and added A* pathfinding to target destinations in the map

--- index.js
+++ index.js
@@ -1,121 +1,142 @@
-const fs = require("fs")
-const {createInterface } = require("readline/promises")
+import fs from "fs";
+import { createInterface } from "readline/promises";
+import { aStar } from "./lib/a_star.js";
 
-const STEP_DELAY = 100 //in ms
-const PATH_TO_MAP = "maps/bigroom.txt" //path to map file
-const START_POS = "6,19"; //"x,y" coordinates to start at, (column,row)
+const STEP_DELAY = 200; //in ms
+const PATH_TO_MAP = "maps/house.txt"; //path to map fil
+// const START_POS = ""; //"x,y" coordinates to start at, (column,row)
 
+async function readMap(pathToMapFile) {
+  const map_matrix = [];
 
-async function readMap(pathToMapFile){
-    const map_matrix = [] 
+  const reader = createInterface({
+    input: fs.createReadStream(pathToMapFile, "utf-8"),
+  });
+  //read in line by line to construct matrix
+  for await (const line of reader) {
+    map_matrix.push(
+      line
+        .split("")
+        .filter((char) => !isNaN(char))
+        .map(Number),
+    );
+  }
 
-    const reader = createInterface({
-        input: fs.createReadStream(pathToMapFile,"utf-8")
-    })
+  const mapGraph = {};
+  for (let i = 0; i < map_matrix.length; i++) {
+    for (let j = 0; j < map_matrix[i].length; j++) {
+      if (map_matrix[i][j] === 1) continue; //do not mark walls
+      //determine neighbor cells
+      const neighbors = [];
+      //cell 'above'
+      if (i > 0 && map_matrix[i - 1][j] === 0) {
+        neighbors.push(`${j},${i - 1}`);
+      }
+      //cell 'below'
+      if (i < map_matrix.length - 1 && map_matrix[i + 1][j] === 0) {
+        neighbors.push(`${j},${i + 1}`);
+      }
 
-    for await ( const line of reader){
-        map_matrix.push(
-            line.split("")
-                .filter((char)=>!isNaN(char))
-                .map(Number)
-        );
+      //cell left
+      if (j > 0 && map_matrix[i][j - 1] === 0) {
+        neighbors.push(`${j - 1},${i}`);
+      }
+
+      //cell right
+      if (j < map_matrix[i].length - 1 && map_matrix[i][j + 1] === 0) {
+        neighbors.push(`${j + 1},${i}`);
+      }
+      mapGraph[`${j},${i}`] = neighbors;
     }
-
-
-    const xMax = map_matrix[0].length-1;
-    const yMax = map_matrix.length-1;
-    console.log(`MAP DIMENSIONS: ${xMax} x ${yMax}`)
-
-    const mapGraph = new Map()
-    for(let i = 0; i < map_matrix.length; i++) {
-        for(let j = 0; j < map_matrix[i].length; j++) {
-            if(map_matrix[i][j] === 1) continue; //do not mark walls
-            const neighbors = []
-            //i is y, j is x
-            
-            //cell 'above' 
-            if(i > 0 && map_matrix[i-1][j] === 0){
-                neighbors.push(`${j},${i-1}`);
-            }
-            //cell 'below'
-            if( i < map_matrix.length-1 && map_matrix[i+1][j] === 0){
-                neighbors.push(`${j},${i+1}`);
-            }
-
-            //cell left
-            if( j > 0 && map_matrix[i][j-1] === 0){
-                neighbors.push(`${j-1},${i}`);
-            }
-
-            //cell right
-            if( j < map_matrix[i].length-1 && map_matrix[i][j+1] === 0){
-                neighbors.push(`${j+1},${i}`)
-            }
-            mapGraph.set(`${j},${i}`,neighbors)
-
-        }
-    }
-    return {map: mapGraph, map_matrix}
+  }
+  return { map: mapGraph, map_matrix };
 }
 
-function printMatrix(matrix,iAt,jAt){
-    let str =""
-    for(let i = 0; i < matrix.length; i++){
-        for(let j = 0; j < matrix[i].length; j++){
-            if(i == iAt && j == jAt){
-                str += "*"
-            }
-            else if(matrix[i][j] == 1) str += "#"
-            else if(matrix[i][j] == 0) str += "•"
-            else str += " "
-        }
-        str += "\n"
-
+function printMatrix(matrix, iAt, jAt) {
+  let str = "";
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i == iAt && j == jAt) {
+        str += "*";
+      } else if (matrix[i][j] == 1) str += "#";
+      else if (matrix[i][j] == 0) str += "•";
+      else str += " ";
     }
-    console.clear()
-    console.log("CURRENTLY AT: ", [iAt,jAt])
-    console.log(str)
+    str += "\n";
+  }
+  if (iAt !== undefined && jAt !== undefined) {
+    console.clear();
+    console.log("CURRENTLY AT: ", [jAt, iAt]);
+  }
+  console.log(str);
 }
 
-const wait = ms => new Promise((resolve)=>setTimeout(resolve,ms))
+function resetMatrix(matrix) {
+  console.time("reset map matrix");
+  if (matrix) {
+    for (let r = 0; r < matrix.length; r++) {
+      for (let c = 0; c < matrix[r].length; c++) {
+        //reset non walls
+        if (matrix[r][c] != 1) matrix[r][c] = 0;
+      }
+    }
+    console.timeEnd("reset map matrix");
+    printMatrix(matrix);
+  }
+}
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 /**
- * 
- * @param {Map<string,string[]>} graph 
+ *
+ * @param {{[key:string]:string[]}} graph
  * @param {string} node the current node
  * @param {string[]} visited list of visited nodes
  */
-async function dfs(graph,node,visited=[],matrix=undefined){
-    if(!graph.has(node)) throw new Error("Encountered invalid node: " + node)
-    await wait(STEP_DELAY)
-    visited.push(node)
-    if(matrix !== undefined){
-        const [j,i] = node.split(",").map(Number)
-        matrix[i][j] = "V"
-        printMatrix(matrix,i,j)
 
+async function dfs(graph, node, visited = [], matrix = undefined) {
+  if (graph[node] === undefined)
+    throw new Error("Encountered invalid node: " + node);
+  await wait(STEP_DELAY);
+  visited.push(node);
+  if (matrix !== undefined) {
+    const [j, i] = node.split(",").map(Number);
+    matrix[i][j] = "V";
+    printMatrix(matrix, i, j);
+  }
+
+  for (const neighbor of graph[node]) {
+    if (!visited.includes(neighbor)) {
+      //navigate from last visit to neighbor
+      const distToNext = aStar(graph, visited.at(-1), neighbor);
+      console.log("'best' path: ", distToNext[1].join(" -> "));
+      console.log("Est. Dist =  ", distToNext[0]);
+      console.log("Actual Moves = ", distToNext[1].length - 1);
+      if (distToNext[0] > 1) {
+        console.log("Waiting for move to target...");
+        await wait(5000);
+      }
+
+      await dfs(graph, neighbor, visited, matrix);
     }
+  }
 
-
-    for(const neighbor of graph.get(node)){
-        if(!visited.includes(neighbor)) 
-        await dfs(graph,neighbor,visited,matrix)
-    }
-    return visited
+  return visited;
 }
 
+async function exploreMap(mapGraph, mapMatrix, pos) {
+  const p = pos || [...mapGraph.keys()][0];
 
-async function main(){
-    const {map,map_matrix} = await readMap(PATH_TO_MAP)
-    const visits = await dfs(map,START_POS,[],map_matrix)
-    console.log(`Visited ${visits.length} cells to traverse entire map!`)
+  const visits = await dfs(mapGraph, p, [], mapMatrix);
+  console.log("# Cells Discovered: ", visits.length);
+  resetMatrix(mapMatrix);
 }
-main()
 
+async function main() {
+  const { map, map_matrix } = await readMap(PATH_TO_MAP);
+  exploreMap(map, map_matrix, "1,1");
 
-
-
-
-
-
-
+  // const visits = await dfs(map, START_POS, [], map_matrix);
+  // console.log(`Visited ${visits.length} cells to traverse entire map!`);
+}
+main();
